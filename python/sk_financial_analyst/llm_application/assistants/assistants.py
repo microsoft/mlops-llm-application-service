f31054077fd85ed19de95771630bee5715a0d7ca
Gromova, Anastasiya (CSW) <Anastasiya.Gromova@Honeywell.com>
2024-12-16 09:11:38 -0800

    linting

--- python/sk_financial_analyst/llm_application/assistants/assistants.py
+++ python/sk_financial_analyst/llm_application/assistants/assistants.py
@@ -11,9 +11,7 @@ import logging
 from assistants.data_models import ConsolidatedReport
 from plugins import plugins as plugins
 from semantic_kernel import Kernel
-from semantic_kernel.connectors.ai.function_choice_behavior import (
-    FunctionChoiceBehavior,
-)
+from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
 from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.azure_chat_prompt_execution_settings import (
     AzureChatPromptExecutionSettings,
@@ -94,9 +92,7 @@ class NewsAnalyst:
 
         # Enable planning
         execution_settings = AzureChatPromptExecutionSettings(tool_choice="auto")
-        execution_settings.function_choice_behavior = FunctionChoiceBehavior.Auto(
-            auto_invoke=True, filters={}
-        )
+        execution_settings.function_choice_behavior = FunctionChoiceBehavior.Auto(auto_invoke=True, filters={})
 
         # Set the chat history
         history = ChatHistory()
@@ -117,9 +113,7 @@ class NewsAnalyst:
 class FinancialAnalyst:
     """A class used to perform analysis of financial statements."""
 
-    def __init__(
-        self, aoai_token, aoai_base_endpoint, llm_deployment_name, sec_identity
-    ):
+    def __init__(self, aoai_token, aoai_base_endpoint, llm_deployment_name, sec_identity):
         """Initialize the FinancialAnalyst class."""
         self.aoai_token = aoai_token
         self.aoai_base_endpoint = aoai_base_endpoint
@@ -187,9 +181,7 @@ class FinancialAnalyst:
 
         # Enable planning
         execution_settings = AzureChatPromptExecutionSettings(tool_choice="auto")
-        execution_settings.function_choice_behavior = FunctionChoiceBehavior.Auto(
-            auto_invoke=True, filters={}
-        )
+        execution_settings.function_choice_behavior = FunctionChoiceBehavior.Auto(auto_invoke=True, filters={})
 
         # Set the chat history
         history = ChatHistory()
@@ -216,18 +208,14 @@ class FinancialAnalyst:
 class StructuredReportGenerator:
     """A class used to generate a consolidated financial report."""
 
-    def __init__(
-        self, aoai_token, aoai_base_endpoint, llm_deployment_name, aoai_api_version
-    ):
+    def __init__(self, aoai_token, aoai_base_endpoint, llm_deployment_name, aoai_api_version):
         """Initialize the ReportGenerator class."""
         self.aoai_token = aoai_token
         self.aoai_base_endpoint = aoai_base_endpoint
         self.llm_deployment_name = llm_deployment_name
         self.aoai_api_version = aoai_api_version
 
-    async def get_consolidated_report(
-        self, balance_sheet_report, income_report, cash_flow_report, news_report
-    ):
+    async def get_consolidated_report(self, balance_sheet_report, income_report, cash_flow_report, news_report):
         """Generate the consolidated financial analysis report."""
         # Define system and user messages
         system_message = """
@@ -287,9 +275,7 @@ class StructuredReportGenerator:
         logging.getLogger("kernel").setLevel(logging.DEBUG)
 
         # Set structured output
-        execution_settings = AzureChatPromptExecutionSettings(
-            response_format=ConsolidatedReport
-        )
+        execution_settings = AzureChatPromptExecutionSettings(response_format=ConsolidatedReport)
 
         # Set the chat history
         history = ChatHistory()
