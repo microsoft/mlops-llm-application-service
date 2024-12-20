5d39f071eb2aa31c5bf9c02ce4dbeb10ed8f86a0
Gromova, Anastasiya (CSW) <Anastasiya.Gromova@Honeywell.com>
2024-12-20 12:53:52 -0800

    commented out financial_health_analysis.py

--- python/sk_financial_analyst/llm_application/financial_health_analysis.py
+++ python/sk_financial_analyst/llm_application/financial_health_analysis.py
@@ -56,65 +56,65 @@ class FinancialHealthAnalysis:
         # Get the news report for the stock ticker
         reports["news_report"] = await news_analyst.get_news_report(stock_ticker=stock_ticker)
 
-        # Create the financial analyst assistant
-        financial_analyst = assistants.FinancialAnalyst(
-            aoai_token=self.aoai_token,
-            aoai_base_endpoint=self.aoai_base_endpoint,
-            llm_deployment_name=self.news_analyst_model,
-            sec_identity=self.sec_identity,
-        )
+        # # Create the financial analyst assistant
+        # financial_analyst = assistants.FinancialAnalyst(
+        #     aoai_token=self.aoai_token,
+        #     aoai_base_endpoint=self.aoai_base_endpoint,
+        #     llm_deployment_name=self.news_analyst_model,
+        #     sec_identity=self.sec_identity,
+        # )
 
-        # Get the financial reports for the stock ticker
+        # # Get the financial reports for the stock ticker
 
-        report_type = "balance_sheet"
-        balance_sheet_report_metrics = """
-            current ratio,
-            quick ratio,
-            working capital,
-            debt to equity ratio
-        """
+        # report_type = "balance_sheet"
+        # balance_sheet_report_metrics = """
+        #     current ratio,
+        #     quick ratio,
+        #     working capital,
+        #     debt to equity ratio
+        # """
 
-        reports["balance_sheet_report"] = await financial_analyst.get_financial_report(
-            stock_ticker=stock_ticker, report_type=report_type, report_metrics=balance_sheet_report_metrics
-        )
+        # reports["balance_sheet_report"] = await financial_analyst.get_financial_report(
+        #     stock_ticker=stock_ticker, report_type=report_type, report_metrics=balance_sheet_report_metrics
+        # )
 
-        report_type = "income"
-        income_report_metrics = """
-            gross margin,
-            profit margin,
-            operating margin,
-            basic earnings per share,
-            basic price to earnings ratio,
-            return on equity
-        """
-        reports["income_report"] = await financial_analyst.get_financial_report(
-            stock_ticker=stock_ticker, report_type=report_type, report_metrics=income_report_metrics
-        )
+        # report_type = "income"
+        # income_report_metrics = """
+        #     gross margin,
+        #     profit margin,
+        #     operating margin,
+        #     basic earnings per share,
+        #     basic price to earnings ratio,
+        #     return on equity
+        # """
+        # reports["income_report"] = await financial_analyst.get_financial_report(
+        #     stock_ticker=stock_ticker, report_type=report_type, report_metrics=income_report_metrics
+        # )
 
-        report_type = "cash_flow"
-        cash_flow_report_metrics = """
-            cash flow per share,
-            free cash flow,
-            cash flow to debt ratio
-        """
-        reports["cash_flow_report"] = await financial_analyst.get_financial_report(
-            stock_ticker=stock_ticker, report_type=report_type, report_metrics=cash_flow_report_metrics
-        )
+        # report_type = "cash_flow"
+        # cash_flow_report_metrics = """
+        #     cash flow per share,
+        #     free cash flow,
+        #     cash flow to debt ratio
+        # """
+        # reports["cash_flow_report"] = await financial_analyst.get_financial_report(
+        #     stock_ticker=stock_ticker, report_type=report_type, report_metrics=cash_flow_report_metrics
+        # )
 
-        # Create the report generator assistant
-        structured_report_generator = assistants.StructuredReportGenerator(
-            aoai_token=self.aoai_token,
-            aoai_base_endpoint=self.aoai_base_endpoint,
-            llm_deployment_name=self.structured_report_generator_model,
-            aoai_api_version=self.aoai_api_version,
-        )
+        # # Create the report generator assistant
+        # structured_report_generator = assistants.StructuredReportGenerator(
+        #     aoai_token=self.aoai_token,
+        #     aoai_base_endpoint=self.aoai_base_endpoint,
+        #     llm_deployment_name=self.structured_report_generator_model,
+        #     aoai_api_version=self.aoai_api_version,
+        # )
 
-        # Generate the structured consolidated report
-        reports["consolidated_report"] = await structured_report_generator.get_consolidated_report(
-            balance_sheet_report=reports["balance_sheet_report"],
-            income_report=reports["income_report"],
-            cash_flow_report=reports["cash_flow_report"],
-            news_report=reports["news_report"],
-        )
+        # # Generate the structured consolidated report
+        # reports["consolidated_report"] = await structured_report_generator.get_consolidated_report(
+        #     balance_sheet_report=reports["balance_sheet_report"],
+        #     income_report=reports["income_report"],
+        #     cash_flow_report=reports["cash_flow_report"],
+        #     news_report=reports["news_report"],
+        # )
 
         return reports
